b3e4508a7d6e9eb2947e20cb086fb01d6e509f1a
nandan kumar <nandanbharadwaj4@gmail.com>
2026-02-12 08:32:26 +0530

    Added basic setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Nandan Kumar",
+    author_email="nandanbharadwaj4@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
